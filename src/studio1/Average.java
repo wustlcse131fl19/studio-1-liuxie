201142a557a2d48907f5dcb452548a64742e1e76
xieyujing <xie.yujing@wustl.edu>
2019-08-29 15:34:38 -0500

    studio 1 finished

---
+++ src/studio1/Average.java
@@ -0,0 +1,14 @@
+package studio1;
+
+import support.cse131.ArgsProcessor;
+
+public class Average {
+    public static void main(String[] args) {
+        ArgsProcessor ap = new ArgsProcessor(args);
+        int n1 = ap.nextInt("The first of two integers to be averaged?");
+        int n2 = ap.nextInt("The second of two integers integer to be averaged?");
+        double ave = (double) (n1 + n2)/2;
+        System.out.println("Average of " + n1 + " and " + n2 + " is " + ave + ".");
+
+    }
+}
